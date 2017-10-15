e428d46d5d7b0fc6b6168b019bc681402cda51f1
Yuriy Nemtsov <nemtsov@gmail.com>
2017-10-14 20:57:28 -0400

    db: add postgres and JWT auth

--- server.js
+++ server.js
@@ -5,19 +5,28 @@ const morgan = require('morgan');
 const config = require('./lib/core/config');
 const loggers = require('./lib/core/loggers');
 const init = require('./lib/core/init');
+const errorHandler = require('./lib/core/errorHandler');
 const githubResource = require('./lib/github/resource');
+const usersResource = require('./lib/users/resource');
+const tokensResource = require('./lib/tokens/resource');
 
 const port = config.get('PORT');
 const logger = loggers.get('server');
 const app = express();
 
-app.use(morgan(':date[iso] (server) INFO: :method :url :remote-addr :response-time :status'));
+app.use(morgan(':date[iso] (server) INFO: :method :url :remote-addr :response-time :status', {
+  skip: (req) => /^\/favicon.ico/.test(req.originalUrl),
+}));
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '10mb' }));
 app.use('/_health', (req, res) => res.end('ok'));
 app.use('/favicon.ico', (req, res) => res.end());
-app.use('/', githubResource);
+app.use('/github', githubResource);
+app.use('/users', usersResource);
+app.use('/tokens', tokensResource);
+
+app.use(errorHandler);
 
 init()
   .catch((err) => {
